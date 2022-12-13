f309106bcef264298942cfa23c12487f75d8988b
soonjung.kwon <bangdy14@gmail.com>
2022-12-13 20:06:23 +0900

    선택 시, 회색 배경색 나오게 (  ternary 쓸 때, "" 이거 쓰지말고, null 써야 함)

--- src/elements/DayBox.js
+++ src/elements/DayBox.js
@@ -1,13 +1,14 @@
-import { Box, Divider, Heading, VStack } from "native-base";
+import { Heading, VStack } from "native-base";
 
 import { DAY_WIDTH_IN_WEEK } from "../constants/layout";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
+import { dateIdGenerator } from "../help/util";
 import dayjs from "dayjs";
 
 export default function Header(props) {
   const { date, selectedDay } = props;
   const yoil = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const isToday = dateIdGenerator(selectedDay) === dateIdGenerator(date);
 
   return (
     <VStack
@@ -16,9 +17,8 @@ export default function Header(props) {
       w={DAY_WIDTH_IN_WEEK}
       mt={2}
       py={2}
-      backgroundColor={dayjs(selectedDay).format("DD/MM/YYYY") === dayjs(date).format("DD/MM/YYYY") ? "amber.100" : ""}
+      backgroundColor={isToday ? "gray.200" : null}
       borderRadius={10}
-      borderWidth={1}
       borderColor="gray.400">
       <Heading size="sm" color={[0, 6].includes(dayjs(date).day()) ? "red.400" : null}>
         {yoil[dayjs(date).day()]}
