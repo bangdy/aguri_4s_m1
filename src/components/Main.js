7c6981d26c6d8f41a2f8e1044fad00df779cddc8
soonjung.kwon <bangdy14@gmail.com>
2022-12-17 16:45:40 +0900

    Tasks, asyncStorage 통하여 사용되게

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 // import { StatusBar } from "expo-status-bar";
-import { Divider, PresenceTransition, Pressable, ScrollView, VStack, useDisclose } from "native-base";
-import React, { useRef, useState } from "react";
+import { Button, Divider, PresenceTransition, Pressable, ScrollView, VStack, useDisclose } from "native-base";
+import React, { useEffect, useRef, useState } from "react";
 
 import { BlurView } from "@react-native-community/blur";
 import DayBox from "../elements/DayBox";
@@ -11,26 +11,40 @@ import TaskWithCarousel from "../elements/TaskWithCarousel";
 import Tickle from "../elements/Tickle";
 import TickleTypeSelectButton from "../elements/TickleTypeSelectButton";
 import WeekCarousel from "../elements/WeekCarousel";
+import { getItemFromAsync } from "../help/util";
 import { trackerArray } from "../constants/naming";
 
-export default function Main() {
+export default function Main(props) {
   const [selectedDay, setSelctedDay] = useState(new Date());
   const [infinityWeekArray, setInfinityWeekArray] = useState([-1, 0, 1]);
   const [prevIndex, setPrevIndex] = useState(1);
 
-  const t1 = new Task("a", "Vitamin");
-  const t2 = new Task("b", "Read a book");
-  const t3 = new Task("c", "Check parent medicine");
-  const tasks = [t1, t2, t3];
-  const refs = [];
+  const [taskCollector, setTaskCollector] = useState([]);
+  const refs = useRef([]);
+  console.log("🚀 ~ file: Main.js:24 ~ Main ~ refs", refs);
 
   const { isOpen, onToggle, onClose } = useDisclose();
 
-  const btnObj = { borderWidth: 1, borderRadius: 100, borderColor: "gray.300", shadow: 6 };
+  const additionalStyles = { borderWidth: 1, borderRadius: 100, borderColor: "gray.300", shadow: 6 };
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const result = await getItemFromAsync("myRoutine");
+      setTaskCollector(result);
+    };
+    fetchTasks();
+  }, []);
 
   return (
     <VStack flex={1} h="100%" bgColor="gray.100" justifyContent="space-between">
       <Header onToggle={onToggle} />
+      <Button
+        colorScheme="primary"
+        onPress={() => {
+          console.log(refs);
+        }}>
+        Primary
+      </Button>
       <VStack h="85%" alignItems="center">
         <WeekCarousel
           selectedDay={selectedDay}
@@ -39,17 +53,16 @@ export default function Main() {
           prevIndex={prevIndex}
           setPrevIndex={setPrevIndex}
           DayItem={DayBox}
-          otherCarousels={refs}
+          otherCarousels={refs} // ref를 통해 함수를 사용하기 위함
         />
         <Divider mt={2} />
         <ScrollView w="100%">
-          {tasks.map((task, i) => {
-            const ref = useRef();
-            refs.push(ref);
+          {taskCollector.map((task, i) => {
             return (
               <TaskWithCarousel
                 key={i}
-                forRef={ref}
+                forwardedRef={refs} // 실제 task 만 ref Array 에 넣기 위한 식별자 1
+                index={i} // 실제 task 만 ref Array 에 넣기 위한 식별자 2
                 enabled={false}
                 selectedDay={selectedDay}
                 infinityWeekArray={infinityWeekArray}
@@ -75,25 +88,27 @@ export default function Main() {
       )}
       <VStack position="absolute" overflow="visible" right={2} top={16}>
         <VStack mr={2} alignItems="flex-end">
-          <Pressable onPress={onToggle} alignItems="center" justifyContent="center" {...(isOpen && btnObj)}>
+          <Pressable onPress={onToggle} alignItems="center" justifyContent="center" {...(isOpen && additionalStyles)}>
             <Ionicons name={isOpen ? "close" : "add"} size={32} color="black" />
           </Pressable>
         </VStack>
-        <VStack justifyContent="flex-start" h={20} opacity={1} mt={2}>
-          <PresenceTransition
-            visible={isOpen}
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: 1,
-              transition: {
-                duration: 250,
-              },
-            }}>
-            {trackerArray.map((moduleName, i) => (
-              <TickleTypeSelectButton moduleName={moduleName} />
-            ))}
-          </PresenceTransition>
-        </VStack>
+        {isOpen && (
+          <VStack justifyContent="flex-start" h={20} opacity={1} mt={2}>
+            <PresenceTransition
+              visible={isOpen}
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  duration: 250,
+                },
+              }}>
+              {trackerArray.map((moduleName, i) => (
+                <TickleTypeSelectButton moduleName={moduleName} key={i} taskCollector={taskCollector} {...props} />
+              ))}
+            </PresenceTransition>
+          </VStack>
+        )}
       </VStack>
     </VStack>
   );
--- src/help/util.js
+++ src/help/util.js
@@ -1,5 +1,5 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import dayjs from "dayjs";
-
 export const infinityWeekCallback = ({
   currentIndex,
   prevIndex,
@@ -25,3 +25,53 @@ export const infinityWeekCallback = ({
 };
 
 export const dateIdGenerator = (date) => dayjs(new Date(date)).format("YYYY.MM.DD");
+
+const isEmpty = function (value) {
+  if (
+    value === "" ||
+    value === null ||
+    value === undefined ||
+    (value !== null && typeof value === "object" && !Object.keys(value).length)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const getItemFromAsync = (storageName) => {
+  if (isEmpty(storageName)) {
+    throw Error("Storage Name is empty");
+  }
+
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem(storageName, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+
+      if (result === null) {
+        resolve(null);
+      }
+
+      resolve(JSON.parse(result));
+    });
+  });
+};
+
+// AsyncStorage set 함수 모듈
+export const setItemToAsync = (storageName, item) => {
+  if (isEmpty(storageName)) {
+    throw Error("Storage Name is empty");
+  }
+
+  return new Promise((resolve, reject) => {
+    AsyncStorage.setItem(storageName, JSON.stringify(item), (error) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve("입력 성공");
+    });
+  });
+};
